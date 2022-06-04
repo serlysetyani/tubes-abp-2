a0d6feb4b26fc6eccd8b4c4ed745f9fc59fed2f3
Serly Setyani <sherly.setyani@gmail.com>
2022-06-04 19:47:45 +0700

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/ngalamblog_flutetr/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.ngalamblog_flutetr
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+import 'package:ngalamblog_flutetr/main_page.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp();
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MainPage(),
+    );
+  }
+}
