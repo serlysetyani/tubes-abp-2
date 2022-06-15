98001b5243476214692505d2bf15e49cbb4e8b28
Serly Setyani <sherly.setyani@gmail.com>
2022-06-15 17:02:27 +0700

    Add Create Articles Feature

--- lib/pages/addArticle_page.dart
+++ lib/pages/addArticle_page.dart
@@ -1,4 +1,8 @@
+// ignore_for_file: prefer_const_constructors, avoid_unnecessary_containers
+
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class AddArticle extends StatefulWidget {
   const AddArticle({Key? key}) : super(key: key);
@@ -8,6 +12,23 @@ class AddArticle extends StatefulWidget {
 }
 
 class _AddArticleState extends State<AddArticle> {
+  final _titleController = TextEditingController();
+  final _contentController = TextEditingController();
+
+  Future addArticles() async {
+    await FirebaseFirestore.instance.collection('articles').add({
+      'title': _titleController.text,
+      'content': _contentController.text,
+    });
+  }
+
+  @override
+  void dispose() {
+    _titleController.dispose();
+    _contentController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,6 +36,15 @@ class _AddArticleState extends State<AddArticle> {
         title: Text('Buat Artikel',
             style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold)),
         elevation: 0,
+        actions: [
+          GestureDetector(
+            onTap: addArticles,
+            child: Container(
+              padding: EdgeInsets.symmetric(horizontal: 20),
+              child: Icon(Icons.file_upload),
+            ),
+          )
+        ],
         backgroundColor: Colors.white,
         iconTheme: IconThemeData(
           color: Colors.black, //change your color here
@@ -28,16 +58,21 @@ class _AddArticleState extends State<AddArticle> {
             SizedBox(
               height: 10,
             ),
-            Container(
-              height: 150,
-              width: MediaQuery.of(context).size.width,
-              child: Icon(
-                Icons.add_a_photo,
-                color: Colors.black,
-              ),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                color: Colors.grey[200],
+            GestureDetector(
+              onTap: () {
+                ImagePicker();
+              },
+              child: Container(
+                height: 150,
+                width: MediaQuery.of(context).size.width,
+                child: Icon(
+                  Icons.add_a_photo,
+                  color: Colors.black,
+                ),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: Colors.grey[200],
+                ),
               ),
             ),
             SizedBox(
@@ -46,11 +81,24 @@ class _AddArticleState extends State<AddArticle> {
             Container(
                 child: Column(
               children: [
+                // Textfield Judul
                 TextField(
                   decoration: InputDecoration(
                       hintText: "Judul",
                       hintStyle: TextStyle(color: Colors.black)),
-                )
+                  controller: _titleController,
+                ),
+                SizedBox(
+                  height: 12,
+                ),
+
+                // Textfield Deskripsi
+                TextField(
+                  decoration: InputDecoration(
+                      hintText: "Deskripsi",
+                      hintStyle: TextStyle(color: Colors.black)),
+                  controller: _contentController,
+                ),
               ],
             ))
           ],
--- lib/pages/homescreen.dart
+++ lib/pages/homescreen.dart
@@ -4,8 +4,6 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:ngalamblog_flutetr/pages/addArticle_page.dart';
 
-import '../pages/inbox_screen.dart';
-
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
