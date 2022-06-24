db9696a642bbcbf8144e278f68d32d8acfdb8751
Serly Setyani <sherly.setyani@gmail.com>
2022-06-24 10:27:49 +0700

    Add Search Field

--- lib/pages/inbox_screen.dart
+++ lib/pages/inbox_screen.dart
@@ -14,18 +14,25 @@ class _InboxScreenState extends State<InboxScreen> {
   Widget build(BuildContext context) {
     return Center(
         child: Scaffold(
-            appBar: AppBar(
-              centerTitle: false,
-              title: Text(
-                "Cari",
-                style:
-                    TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
-              ),
-              backgroundColor: Colors.white,
-              elevation: 0,
-            ),
-            body: Center(
-              child: Text("data"),
-            )));
+      appBar: AppBar(
+        centerTitle: false,
+        title: Text(
+          "Cari",
+          style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
+        ),
+        backgroundColor: Colors.white,
+        elevation: 0,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: TextField(
+          decoration: InputDecoration(
+              hintText: 'Cari',
+              prefixIcon: Icon(Icons.search),
+              border:
+                  OutlineInputBorder(borderRadius: BorderRadius.circular(10))),
+        ),
+      ),
+    ));
   }
 }
